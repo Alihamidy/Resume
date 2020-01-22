afe9e0bb653312c85b97e5e49b2e11ea2aa65d85
Ali HAMIDY <44427807+Alihamidy@users.noreply.github.com>
2020-01-23 00:11:20 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>C.V Ali Hamidy</title>
+
+	<meta name="author" content="Ali Hamidy">
+	<meta name="description" content="Freelancer Concepteur Développeur Informatique et Développeur Web Full-Stack">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Style bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!--  Font awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+	<!--  bootstrap Social buttons -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700|Comfortaa:400,700|Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+
+	<!-- Animate -->
+	<link rel="stylesheet" href="css/animate.css">
+
+	<!-- Hovereffect -->
+	<link rel="stylesheet" href="css/hovereffect.css">
+
+	<!-- Fancybox -->
+	<link rel="stylesheet" href="css/fancybox/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+
+	<!-- Fancybox helpers -->
+	<link rel="stylesheet" href="css/fancybox/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/fancybox/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+
+	<!-- Style perso -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body id="acceuil" data-spy="scroll" data-target=".navbar-fixed-top">
+
+	<!-- header -->
+	<header id="header">
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header page-scroll">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a class="page-scroll text-uppercase" href="#acceuil">Accueil</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#presentation">Présentation</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#competences">Compétences</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#realisations">Réalisations</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#experiences">Expériences</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#formations">Formations</a>
+						</li>
+						<li>
+							<a class="page-scroll text-uppercase" href="#contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<div id="header_content" class="wow fadeIn">
+			<h1>Ali Hamidy</h1>
+			<h2>Concepteur Développeur Informatique, Recherche d'un emploi</h2>
+		</div>
+	</header>
+	<!-- /header -->
+
+	<!-- Partie Présentation -->
+	<div class="section section_blanche container-fluid">
+		<section class="container" id="presentation">
+			<h2 class="text-center text-uppercase">Présentation</h2>
+
+			<div class="row">
+				<div class="col-md-offset-2 col-md-4 text-center wow fadeInLeft" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<img src="img/avatar1.png" id="img_avatar" alt="Présentation" class="img-responsive">
+				</div>
+				<div class="col-md-6 wow fadeInRight" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<ul>
+						<li><i class="fa fa-user" aria-hidden="true"></i> Identité: Ali HAMIDY</li>
+						<li><i class="fa fa-birthday-cake" aria-hidden="true"></i> Age: 34 ans</li>
+						<li><i class="fa fa-male" aria-hidden="true"></i> Situation familiale: Marié</li>
+						<li><i class="fa fa-home" aria-hidden="true"></i> Logement: Montauban (82000)</li>
+						<li><i class="fa fa-car" aria-hidden="true"></i> Transport: Permis de conduire B et véhicule personnel</li>
+						<li><i class="fa fa-hourglass" aria-hidden="true"></i> Loisirs
+							<ul>
+								<li>Jeux Vidéos</li>
+								<li>Séries / Animation</li>
+								<li>Lire</li>
+							</ul>
+						</li>
+						<li><i class="fa fa-flag" aria-hidden="true"></i> Qualités / Défauts
+							<ul>
+								<li>Travail en équipe</li>
+								<li>Calme</li>
+							</ul>
+						</li>
+				</div>
+			</div>
+
+		</section>
+	</div>
+	<!-- /Partie Présentation -->
+
+	<!-- Partie Compétences -->
+	<div class="section section_grise container-fluid">
+		<section class="container" id="competences">
+			<h2 class="text-center text-uppercase">Compétences</h2>
+			<div class="row">
+
+				<!-- Java -->
+				<div class="col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<div class="content_comp hovereffect">
+						<div class="ribbon_css"><span>+ d'infos !</span></div>
+						<div class="rank">
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+						</div>
+						<img class="logo" src="img/Java.png" alt="Java_logo">
+						<h3>Java</h3>
+						<p>Pendant ma formation, j’ai appris Java et j’ai trouvé que Java était un langage informatique très facile à utiliser et compréhensible.</p>
+						<div class="overlay">
+				            <h2>Infos complémentaires</h2>
+							<p>programmation Objet</p>
+				            <ul>
+								<li>JDBC</li>
+							</ul>
+							<p>SWING</p>
+							<ul>
+								<li>Réseau 3</li>
+							</ul>
+				        </div>
+					</div>
+				</div>
+
+				<!-- Java J2e -->
+				<div class="col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<div class="content_comp hovereffect">
+						<div class="ribbon_css"><span>+ d'infos !</span></div>
+						<div class="rank">
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+						</div>
+						<img class="logo" src="img/Java_J2e.png" alt="Java_J2e_logo">
+						<h3>Java J2ee</h3>
+						<p>Utilisation de Java J2e pour programmation Dynamique Web App.</p>
+						<div class="overlay">
+				            <h2>Infos complémentaires</h2>
+							<p>Programmation Web App</p>
+				            <ul>
+								<li>Servlet,JSP</li>
+							</ul>
+							<p>Spring</p>
+							<ul>
+								<li>Pools de connexions</li>
+							</ul>
+				        </div>
+					</div>
+				</div>
+
+				<!-- Front-End -->
+				<div class="col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<div class="content_comp hovereffect">
+						<div class="ribbon_js"><span>+ d'infos !</span></div>
+						<div class="rank">
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-half-o fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+						</div>
+						<img class="logo" src="img/quad_logos.png" alt="Front-End_logo">
+						<h3>Front-End</h3>
+						<p>Jamais du HTML sans son CSS, indispensable pour styliser les pages web, Utiliser BootStrap donne un aspect plus parfait.</p>
+						<div class="overlay">
+							<h2>Infos complémentaires</h2>
+							<p>HTML 5</p>
+							<p>CSS</p>
+							<ul>
+								<li>JavaScript</li>
+								<li>BootStrap</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<!-- PHP -->
+				<div class="col-md-offset-2 col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<div class="content_comp hovereffect">
+						<div class="ribbon_php"><span>+ d'infos !</span></div>
+						<div class="rank">
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-half-o fa-lg" aria-hidden="true"></i>
+						</div>
+						<img class="logo" src="img/php.png" alt="php_logo">
+						<h3>PHP</h3>
+						<p>L'utilisation de PHP offre aux développeurs Web la liberté de choisir leur système d'exploitation et leur serveur Web.
+							En PHP, les scripts côté serveur constituent le principal domaine d’exploitation. Les scripts côté serveur avec PHP impliquent:
+							Analyseur PHP, serveur Web, navigateur Web.</p>
+						<div class="overlay">
+							<h2>Infos complémentaires</h2>
+							<p>Méthodes de programmation</p>
+							<ul>
+								<li>Procédural</li>
+								<li>Orienté Objet</li>
+							</ul>
+							<p>Frameworks PHP</p>
+							<ul>
+								<li>Symfony 3 </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<!-- Database -->
+				<div class="col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<div class="content_comp">
+						<div class="rank">
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+						</div>
+						<img class="logo" src="img/Database.png" alt="Database_logo">
+						<h3>Base de données</h3>
+						<p>Je connais différentes bases de données. Mais la plupart du temps, j'utilise MySQL et Oracle SQL Development.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<!-- /Partie Compétences -->
+
+	<!-- Partie Projets -->
+	<div class="section section_blanche container-fluid">
+		<section class="container" id="realisations">
+			<h2 class="text-center text-uppercase">Réalisations</h2>
+			<h3 class="text-center">Projets auxquels j'ai participé</h3>
+
+			<div class="row wow fadeIn">
+				<div class="col-md-6">
+	               <a class="fancybox" rel="group" href="img/LBPL.png"><img src="img/LBPL.png" alt="LBPL" class="img-responsive"></a>
+	            </div>
+
+				<div class="col-md-6">
+	                <h3><a href="http://www.lbpl.fr/" title="Visiter lbpl">Les Bons P'tits Loups</h3></a>
+	                <p>Site Web et application Web pour la garderie d'enfants.
+	                <p>Dans le cadre de mes études à l’AFPA, j’ai dû faire un stage de 3 mois pour valider mon diplôme. J'ai conçu le site Web et l'application Web pour l'Association Les Bons P'tits Loups. Cette association est chargée de s'occuper des enfants pendant les périodes scolaires et les vacances scolaires. c'est plus comme une garderie.</p>
+	            </div>
+			</div>
+
+		</section>
+	</div>
+	
+	<div class="section section_blanche container-fluid">
+		<section class="container" id="realisations">
+			
+
+			<div class="row wow fadeIn">
+				<div class="col-md-6">
+	               <a class="fancybox" rel="group" href="img/HRM.png"><img src="img/HRM.png" alt="LBPL" class="img-responsive"></a>
+	            </div>
+
+				<div class="col-md-6">
+	                <h3><a href="https://sobhansoft.com/" title="Visiter lbpl">Sobhan Soft</h3></a>
+	                <p>Développeur Java Aplication Gestion des ressources humaines.
+	                <p>Je développe une application Java pour la gestion des ressources humaines en
+						tant que pigiste pour SobhanSoft en Afghanistan. Actuellement, le projet est en
+						phase finale et je recherche tout bogue dans l'application avant de le soumettre
+						au client.</p>
+	            </div>
+			</div>
+
+		</section>
+	</div>
+	<!-- /Partie Projets -->
+
+	<!-- Partie Expériences Professionnelles -->
+	<div class="section section_grise container-fluid">
+		<section class="container" id="experiences">
+			<h2 class="text-center text-uppercase">Expériences Professionnelles</h2>
+			<div class="row">
+				<div class="col-md-4 text-center wow fadeInLeft" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<img src="img/exp_pro.jpg" id="img_exp_pro" alt="Expériences Professionnelles" class="img-responsive">
+				</div>
+				<div class="col-md-8 wow fadeInRight" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<ul>
+						<li><span class="color_date">2019-2019</span> Développeur Application (Java-MySQL), <a href="http://www.sobhansoft.com/" target="_blank">Sobhan Soft, Kaboul, Afghanistan.Travail à domicile.</a></li><br>
+						<li><span class="color_date">2019-2019</span> Développeur Web- Full Stack (PHP-MySQL), <a href="http://www.lbpl.fr/" target="_blank">Association Bons P’tits Loup, Toulouse, France.</a></li><br>
+						<li><span class="color_date">2013-2015</span> Support Informatique, <a href="http://mopw.gov.af/en/" target="_blank">Ministère des Travaux Publics, Secteur Public, Kaboul, Afghanistan.</a></li><br>
+						<li><span class="color_date">2011-2013</span> Directeur Marketing, <a href="http://raditechnology.com/" target="_blank">Radi Technologies, Programmation, conception Web, Inde et Afghanistan.</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	</div>
+	<!-- /Partie Expériences Professionnelles -->
+
+	<!-- Partie Formation -->
+	<div class="section section_blanche container-fluid">
+		<section class="container" id="formations">
+			<h2 class="text-center text-uppercase">Formations</h2>
+			<div class="row">
+				<div class="col-md-8 wow fadeInLeft" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<ul>
+						
+						<li><span class="color_date">2018-2019</span> Concepteur Développeur informatique Titre Professionnel (bac+4), AFPA, BALMA, Toulouse.</a></li><br>
+						<li><span class="color_date">2008-2011</span> Diplôme d'Administration d'Entreprise (équivalent bac+3), Universite de Goa, Inde.</li><br>
+						<li><span class="color_date">2009-2011</span> Diplôme de technicien en informatique (équivalent bac+2), Institute Indien de Technologie (Formation en ligne), Goa, Inde.</li><br>
+						
+				</div>
+				<div class="col-md-4 text-center wow fadeInRight" data-wow-duration="0.7s" data-wow-delay="0.2s">
+					<img src="img/formation.png" id="img_formation" alt="Formation" class="img-responsive">
+				</div>
+			</div>
+		</section>
+	</div>
+	<!-- /Partie Formation -->
+
+	<!-- Partie Contact -->
+	<div class="section section_grise container-fluid">
+		<section class="container text-center" id="contact">
+	        <div class="row">
+	            <div class="col-lg-12">
+	                <h3 class="text-uppercase">Contact</h3>
+
+	                <div class="wow fadeInDown" data-wow-delay="0.7s">
+	                    <h4>Mon profil vous intéresse ?</h4>
+	                    <p>N'hésitez pas à me contacter par email ou par les réseaux sociaux.</p>
+	                </div>
+					<br>
+
+	                <div class="col-md-6 wow fadeIn" data-wow-delay="1s">
+						<a href="mailto:hamidy.ali.fawad@gmail.com"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+						<p>admin@radtechs.net</p></a>
+					</div>
+					<div class="col-md-offset-1 col-md-3 wow fadeIn" data-wow-delay="1s">
+						<!-- <a class="btn btn-block btn-twitter" href="https://twitter.com/Lex24290">
+					    	<span class="fa fa-twitter"> Twitter</span>
+						</a> -->
+						<a class="btn btn-block btn-linkedin" href="https://linkedin.com/in/ali-hamidy-74278b176/" target="_blank">
+					    	<span class="fa fa-linkedin"> LinkedIn</span>
+						</a>
+						<a class="btn btn-block btn-github" href="https://github.com/Alihamidy" target="_blank">
+					    	<span class="fa fa-github"> Github</span>
+						</a>
+	                </div>
+
+	            </div>
+	        </div>
+		</section>
+	</div>
+	<!-- /Partie Contact -->
+
+	<footer id="footer">
+
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<!-- Fancybox mousewheel plugin -->
+	<script type="text/javascript" src="js/jquery.mousewheel-3.0.6.pack.js"></script>
+
+	<!-- FancyBox -->
+	<script type="text/javascript" src="js/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+	<!-- Fancybox helpers -->
+	<script type="text/javascript" src="js/jquery.fancybox-buttons.js?v=1.0.5"></script>
+	<script type="text/javascript" src="js/jquery.fancybox-media.js?v=1.0.6"></script>
+	<script type="text/javascript" src="js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+</html>
